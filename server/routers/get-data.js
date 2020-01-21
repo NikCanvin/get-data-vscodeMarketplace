a2948b371fec7a3a80ef0b8020c5098bce82ad3d
Nik Canvin <nik_canvin@uk.ibm.com>
2020-01-21 11:36:28 +0000

    first release

--- server/routers/get-data.js
+++ server/routers/get-data.js
@@ -1,6 +1,35 @@
 const express = require('express');
 var request = require('request');
 const timestamp = require('time-stamp');
+const htmlparser2 = require("htmlparser2");
+var metrics = {};
+
+const parser = new htmlparser2.Parser(
+  {
+      onopentag(name, attribs) {
+          if (name === "script" && attribs.type === "application/json") {
+          }
+      },
+      ontext(text) {
+          if (text.includes("statistics")) {
+              //console.log("-->", text);
+              const obj = JSON.parse(text);
+              // console.log("Vscode Marketplace metrics:")
+              // console.log(timestamp.utc('YYYY/MM/DD:HH:mm:ss'));
+              // console.log(obj.statistics);
+              metrics.campaign = "Codewind";
+              metrics.getDataType = "VSCodePluginMarketplaceMetrics"
+              metrics.dataCreatedTimestamp = timestamp.utc('YYYY/MM/DD:HH:mm:ss');
+              metrics.metrics = obj.statistics;
+          }
+      },
+      onclosetag(tagname) {
+          if (tagname === "script") {
+          }
+      }
+  },
+  { decodeEntities: true }
+);
 
 module.exports = function (app) {
   const router = express.Router();
@@ -9,61 +38,21 @@ module.exports = function (app) {
 
     request({
 
-       uri: "https://marketplace.eclipse.org/content/codewind#group-metrics-tab",
-
+      uri: "https://marketplace.visualstudio.com/items?itemName=IBM.codewind",
+  
     }, function(error,response,body){
-
-        var bodyArray = body.split(/\r?\n/);
-        var foundMetrics="no";
-        var gotData;
-        var dataCreatedTimestamp = timestamp.utc('YYYY/MM/DD:HH:mm:ss');
-        //gotData = "Eclipse Marketplace metrics: "+timestamp.utc('YYYY/MM/DD:HH:mm:ss');
-        var jsonOutput = { campaign: "Codewind", getDataType: "EclispePluginMarketplaceMetrics" };
-        jsonOutput.dataCreatedTimestamp = timestamp.utc('YYYY/MM/DD:HH:mm:ss');
-        jsonOutput.metrics={};
-        monthNumber=0;
-        for (i = 0; i < bodyArray.length; i++) { // find start of metrics data table in the html
-            if ( bodyArray[i].includes("<thead><tr><th>Date</th><th>Ranking</th><th>Installs</th><th>Clickthroughs</th> </tr></thead>")) {
-                foundMetrics="yes";
-                //console.log("line"+i+" "+bodyArray[i]);
-            }
-            if ( foundMetrics=="yes" ) {
-                if ( bodyArray[i].includes("</table>") ) {
-                    foundMetrics="no";
-                } else {
-                    if ( bodyArray[i].includes("thead") || bodyArray[i].includes("tbody")) { //ignore table header and body rows
-                    } else {
-                        // split string on <td>
-                        gotData += bodyArray[i]+",";
-                        var splitBody = bodyArray[i].split("<td>");
-                        var month = splitBody[1].substring(0,splitBody[1].length-5);
-                        var rankingData = splitBody[2].substring(0,splitBody[2].length-5);
-                        var rankingDataPart = rankingData.split("/");
-                        var rankingCurrentValue = rankingDataPart[0];
-                        var rankingOfTotalPlugins = rankingDataPart[1];
-                        //var rankingDataObject = { rankingCurrentValue: rankingCurrentValue, rankingOfTotalPlugins: rankingOfTotalPlugins };
-                        var installsData = splitBody[3].substring(0,splitBody[3].length-5);
-                        var installsDataPart = installsData.split(" ");
-                        var installsCurrentValue = installsDataPart[0];
-                        var percentageOfAllInstalls = installsDataPart[1].substring(1,installsDataPart[1].length-2);
-                        //var installsDataObject = { installsCurrentValue: installsCurrentValue, percentageOfAllInstalls: percentageOfAllInstalls}
-                        var clickThroughs = splitBody[4].substring(0,splitBody[4].length-11);
-                        var monthValue = monthNumber.toString();
-                        var monthlyMetrics = { month: month, rankingCurrentValue: rankingCurrentValue, rankingOfTotalPlugins: rankingOfTotalPlugins, installsCurrentValue: installsCurrentValue, percentageOfAllInstalls: percentageOfAllInstalls, clickThroughs: clickThroughs };
-                        console.log(monthlyMetrics);
-                        jsonOutput.metrics[monthNumber] = monthlyMetrics;
-                        //jsonOutput.month = month;
-                        var monthNumber = monthNumber+1;
-                    }
-                }
-            }
-        }
-        res.json(jsonOutput);
-        //res.status(200).send(gotData);
-        }
-    );
-
+    
+        //console.log("hi");
+        parser.write(
+            body, metrics
+        );
+        parser.end();
+    
+        res.status(200).send(metrics);
+    
     });
+
+  });
 
   app.use('/get-data', router);
 }
